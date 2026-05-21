850264e827474f2fcbd019b9345cca8bd68d4bc9
Nguyễn Văn Luyến <nguyenluyen@nsg.edu.vn>
2026-05-22 06:44:14 +0700

    fix: remove outdated JS replace script in frontend

--- src/pages/check.js
+++
@@ -1,1 +0,0 @@
-const fs = require('fs'); const content = fs.readFileSync('src/pages/index.tsx', 'utf8'); console.log(content.substring(content.indexOf('useEffect(() => {'), content.indexOf('const MajorCard')));
